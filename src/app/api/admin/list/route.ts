dfe471ff359f5dcecb058e89ffb16c3f3f728503
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2023-10-11 21:53:47 -0400

    debug some stuff

--- src/app/api/admin/list/route.ts
+++ src/app/api/admin/list/route.ts
@@ -4,12 +4,5 @@ import getQrCodesModel from "@/database/qr-codes";
 export async function GET(request: Request) {
     const QrCodeModel = await getQrCodesModel();
     const all = await QrCodeModel.find();
-    return NextResponse.json(all, {
-        status: 200,
-        headers: {
-            'Cache-Control': 'public, s-maxage=1',
-            'CDN-Cache-Control': 'public, s-maxage=1',
-            'Vercel-CDN-Cache-Control': 'public, s-maxage=1',
-        }
-    })
+    return NextResponse.json({all,unixMillis: Date.now()})
 }
