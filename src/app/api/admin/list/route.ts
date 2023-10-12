88875220f0f99c2a762327cf6bde8215cc3ce568
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2023-10-11 22:00:12 -0400

    hjdfjdhsfjsdh

--- src/app/api/admin/list/route.ts
+++ src/app/api/admin/list/route.ts
@@ -4,7 +4,7 @@ import getQrCodesModel from "@/database/qr-codes";
 export async function GET(request: Request) {
     const QrCodeModel = await getQrCodesModel();
     const all = await QrCodeModel.find();
-    return NextResponse.json(all, {
+    return NextResponse.json({all, timestamp: Date.now()}, {
         status: 200,
         headers: {
             "Cache-Control": "no-cache"
