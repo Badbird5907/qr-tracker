a34e6350a071d68ab312333840f4aa4c483e0cb7
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2023-10-11 21:55:41 -0400

    Disable cache

--- src/app/api/admin/list/route.ts
+++ src/app/api/admin/list/route.ts
@@ -4,5 +4,10 @@ import getQrCodesModel from "@/database/qr-codes";
 export async function GET(request: Request) {
     const QrCodeModel = await getQrCodesModel();
     const all = await QrCodeModel.find();
-    return NextResponse.json({all,unixMillis: Date.now()})
+    return NextResponse.json({all}, {
+        status: 200,
+        headers: {
+            "cache-control": "no-cache"
+        }
+    })
 }
