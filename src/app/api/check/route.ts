b6592923201e495f7c8ac9c1cc4502ed02b24b11
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2024-11-26 09:13:38 -0500

    feat: add /api/check endpoint

---
+++ src/app/api/check/route.ts
@@ -0,0 +1,26 @@
+import getQrCodesModel from "@/database/qr-codes";
+import {NextResponse} from "next/server";
+
+export const fetchCache = 'force-no-store';
+export const revalidate = 0;
+export const dynamic = "force-dynamic";
+export async function GET(request: Request) {
+  // get ?slug=xxx
+  const url = new URL(request.url);
+  const slug = url.searchParams.get("slug");
+  if (!slug) {
+    return NextResponse.json({
+      message: "slug is required",
+    }, {status: 400});
+  }
+  const QrCodeModel = await getQrCodesModel();
+  const qrCode = await QrCodeModel.findOne({slug});
+  if (!qrCode) {
+    return NextResponse.json({
+      message: "qr code not found",
+    }, {status: 404});
+  }
+  return NextResponse.json({
+    message: "ok",
+  });
+}
